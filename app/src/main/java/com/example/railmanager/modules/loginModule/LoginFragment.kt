5ffe1ef4b62be972154fea9d76a698c154af9f2d
MarcoCampa47 <151619334+MarcoCampa47@users.noreply.github.com>
2024-06-06 11:44:26 +0200

    Aggiunte dipendenze e fragmentManager
    1) Aggiunta:
    - Dipendenza Navigation
    - Fragment Manager LoginFragment
    
    2) Rimosso:
    - File ccc.kt di test.
    
    3) Modificato id fragment container della main activity

--- app/src/main/java/com/example/railmanager/modules/loginModule/LoginFragment.kt
+++ app/src/main/java/com/example/railmanager/modules/loginModule/LoginFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.railmanager.R
+import com.example.railmanager.modules.registrationModule.RegistrationFragment
 
 class LoginFragment : Fragment() {
 
@@ -14,6 +18,24 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.loginfragment_layout, container, false)
+        val viewWithfragment = inflater.inflate(R.layout.loginfragment_layout, container, false)
+
+        val registerButton = viewWithfragment.findViewById<Button>(R.id.notRegisteredButtonLoginFragment)
+        registerButton.setOnClickListener(){
+            val loginFragmentManager = requireActivity().supportFragmentManager
+            val loginFragmentTransaction = loginFragmentManager.commit {
+                setReorderingAllowed(true)
+                /*Aggiungo al backstack la transazione (il nome è a piacere), così da poter tornare alla schermata precedente col tasto indietro del sistema operativo*/
+                addToBackStack("move_to_register_form")
+                setCustomAnimations(    com.google.android.material.R.anim.m3_side_sheet_enter_from_right,
+                    com.google.android.material.R.anim.m3_side_sheet_exit_to_left,
+                    com.google.android.material.R.anim.m3_side_sheet_enter_from_left,
+                    com.google.android.material.R.anim.m3_side_sheet_exit_to_right)
+                replace<RegistrationFragment>(R.id.fragmentContainerActivityMain)
+            }
+        }
+
+        return viewWithfragment;
     }
+
 }

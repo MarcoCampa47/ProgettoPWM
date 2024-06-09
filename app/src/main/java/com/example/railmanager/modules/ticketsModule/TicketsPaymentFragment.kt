1b549c0606b55388f9e48ea476f31faa56668c02
francesco.settecase <settecasefrancesco0@gmail.com>
2024-06-09 19:19:17 +0200

    TicketsPayment fragment
    aggiunti:
    
    1) Fragment per la scelta e il pagamento del biglietto
    2)immagine per il bottone indietro per il fragment del pagamento del biglietto
    3)recyclerView
    4)immagini varie

---
+++ app/src/main/java/com/example/railmanager/modules/ticketsModule/TicketsPaymentFragment.kt
@@ -0,0 +1,39 @@
+package com.example.railmanager.modules.ticketsModule
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.railmanager.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class TicketsPaymentFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tickets_payment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = CustomAdapter(arrayListOf(1,2,3,4,5,6,7,8,9,10))
+
+        val recyclerView : RecyclerView = view.findViewById(R.id.rvFragmentTicketsPayment)
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+    }
+}

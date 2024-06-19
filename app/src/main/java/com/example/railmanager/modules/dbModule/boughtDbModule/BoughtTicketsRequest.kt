d97f6ddbbe6a2d0b1c9523b24b2db759fec6ef5c
MarcoCampa47 <151619334+MarcoCampa47@users.noreply.github.com>
2024-06-19 13:17:25 +0200

    Implementata logica visualizzazione dettagli biglietto

--- app/src/main/java/com/example/railmanager/modules/dbModule/boughtDbModule/BoughtTicketsRequest.kt
+++ app/src/main/java/com/example/railmanager/modules/dbModule/boughtDbModule/BoughtTicketsRequest.kt
@@ -1,13 +1,16 @@
 package com.example.railmanager.modules.dbModule.boughtDbModule
 
-data class BoughtTicketsRequest (
+import android.os.Parcelable
 
+import android.os.Parcel
+
+data class BoughtTicketsRequest(
     val iduser_ticket: Long,
     val train_name: String,
     val train_type: String,
     val idstate: Int,
     val state_description: String,
-    val class_number : Short,
+    val class_number: Short,
     val departure_time: String,
     val arrival_time: String,
     val departure_city_name: String,
@@ -21,5 +24,62 @@ data class BoughtTicketsRequest (
     val minors_number: Int,
     val adults_price: Double,
     val minors_price: Double
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readInt().toShort(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readDouble(),
+        parcel.readDouble()
+    )
 
-)
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(iduser_ticket)
+        parcel.writeString(train_name)
+        parcel.writeString(train_type)
+        parcel.writeInt(idstate)
+        parcel.writeString(state_description)
+        parcel.writeInt(class_number.toInt())
+        parcel.writeString(departure_time)
+        parcel.writeString(arrival_time)
+        parcel.writeString(departure_city_name)
+        parcel.writeString(arrival_city_name)
+        parcel.writeString(departure_station_name)
+        parcel.writeString(arrival_station_name)
+        parcel.writeInt(departure_station_id)
+        parcel.writeInt(arrival_station_id)
+        parcel.writeString(date_purchase)
+        parcel.writeInt(adults_number)
+        parcel.writeInt(minors_number)
+        parcel.writeDouble(adults_price)
+        parcel.writeDouble(minors_price)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<BoughtTicketsRequest> {
+        override fun createFromParcel(parcel: Parcel): BoughtTicketsRequest {
+            return BoughtTicketsRequest(parcel)
+        }
+
+        override fun newArray(size: Int): Array<BoughtTicketsRequest?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
